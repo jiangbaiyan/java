144060a5124e988e948c2a502880285be06a6664
jiangbaiyan <jiangbaiyan@jd.com>
2020-08-03 19:01:49 +0800

    日期时间

--- src/com/nosay/DateDemo.java
+++ src/com/nosay/DateDemo.java
@@ -1,11 +1,24 @@
 package src.com.nosay;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateDemo {
-    public static void main(String[] args) {
-        Date date = new Date(); // 很多方法都过气了，用Calender类代替了
+    public static void main(String[] args) throws Exception {
+        // Date类很多方法都过气了，用Calender类代替了
+        Date date = new Date();
         System.out.println(date); // Sun Aug 02 19:07:42 CST 2020
         System.out.println(date.getTime()); // 时间戳，相当于php的time()
+        // DateFormat - 日期时间格式化
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(dateFormat.format(date));
+        System.out.println(dateFormat.parse("2020-8-3 18:55:51"));
+        // Calendar类 - 常用
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
+        System.out.println(calendar.get(Calendar.SECOND));
+        System.out.println(calendar.get(Calendar.YEAR));
     }
 }
