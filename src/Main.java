8b9da9823a7b6282d3da165a1d16c11b9671e749
jiangbaiyan <987082332@qq.com>
2020-08-03 22:33:30 +0800

    枚举类

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+import src.com.nosay.EventEnum;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EventEnum ee = EventEnum.LAUNCH;
+        ee.show();
+    }
+}
---
+++ src/com/nosay/EventEnum.java
@@ -0,0 +1,21 @@
+package src.com.nosay;
+
+public enum EventEnum {
+
+    // 构造方法直接定义
+    LAUNCH("launch"), PAGEVIEW("pageview"), EVENT("event");
+
+    EventEnum(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public void show() {
+        System.out.println(this.name);
+        EventEnum[] ee = values(); // 返回所有枚举值
+        for (int i = 0; i < ee.length; i++) {
+            System.out.println(ee[i]);
+        }
+    }
+}
