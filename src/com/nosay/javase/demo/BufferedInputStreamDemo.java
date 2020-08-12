5323f26bfa397ce6ef6e9066c86dcef06fb76f7a
jiangbaiyan <987082332@qq.com>
2020-08-12 21:07:40 +0800

    缓冲流

---
+++ src/com/nosay/javase/demo/BufferedInputStreamDemo.java
@@ -0,0 +1,24 @@
+package src.com.nosay.javase.demo;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+public class BufferedInputStreamDemo {
+    public static void main(String[] args) {
+        FileInputStream fileInputStream = null;
+        BufferedInputStream bufferedInputStream = null;
+        // 缓冲流
+        // transient关键字：序列化的时候不会序列化该字段
+        try {
+            fileInputStream = new FileInputStream("abc.txt");
+            bufferedInputStream = new BufferedInputStream(fileInputStream);
+            int read = 0;
+            while ((read = bufferedInputStream.read()) != -1) {
+                System.out.println((char)read);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
