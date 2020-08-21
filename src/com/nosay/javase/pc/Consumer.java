229843cb65792eeeeefe9a9cec041e8f19d940d9
baynejiang <baynejiang@tencent.com>
2020-08-21 21:15:54 +0800

    生产者消费者模型

---
+++ src/com/nosay/javase/pc/Consumer.java
@@ -0,0 +1,17 @@
+package src.com.nosay.javase.pc;
+
+public class Consumer implements Runnable {
+
+    private Goods goods;
+
+    public Consumer(Goods goods) {
+        this.goods = goods;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            this.goods.get();
+        }
+    }
+}
---
+++ src/com/nosay/javase/pc/Producer.java
@@ -0,0 +1,22 @@
+package src.com.nosay.javase.pc;
+
+public class Producer implements Runnable {
+
+    private Goods goods;
+
+    public Producer(Goods goods) {
+        this.goods = goods;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10 ; i++) {
+            // 生产娃哈哈或者矿泉水
+            if (i % 2 == 0) {
+                this.goods.set("wahaha", "kuangquanshui");
+            } else {
+                this.goods.set("wangzai", "xiaomantou");
+            }
+        }
+    }
+}
