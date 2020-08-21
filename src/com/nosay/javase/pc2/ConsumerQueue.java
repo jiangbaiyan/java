89c1ecdea48ff1449ed1dcf85db7720dc74f4c58
baynejiang <baynejiang@tencent.com>
2020-08-21 22:49:30 +0800

    JUC

---
+++ src/com/nosay/javase/pc2/ConsumerQueue.java
@@ -0,0 +1,24 @@
+package src.com.nosay.javase.pc2;
+
+import java.util.concurrent.BlockingQueue;
+
+public class ConsumerQueue implements Runnable {
+
+    private BlockingQueue blockingQueue;
+
+    public ConsumerQueue(BlockingQueue blockingQueue) {
+        this.blockingQueue = blockingQueue;
+    }
+
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                System.out.println("消费者消费的是:" + blockingQueue.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/com/nosay/javase/pc2/ProducerQueue.java
@@ -0,0 +1,26 @@
+package src.com.nosay.javase.pc2;
+
+import java.util.concurrent.BlockingQueue;
+
+public class ProducerQueue implements Runnable {
+
+    private BlockingQueue blockingQueue;
+
+    public ProducerQueue(BlockingQueue blockingQueue) {
+        this.blockingQueue = blockingQueue;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("生产者开始生产商品:" + i);
+            try {
+                this.blockingQueue.put(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
