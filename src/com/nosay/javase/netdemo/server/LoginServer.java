2276741fff95570f9f2931739549951888d72d94
baynejiang <baynejiang@tencent.com>
2020-08-23 12:33:29 +0800

    网络编程

--- src/com/nosay/javase/netdemo/server/LoginServer.java
+++ src/com/nosay/javase/netdemo/server/LoginServer.java
@@ -9,27 +9,30 @@ import java.net.Socket;
 public class LoginServer {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         ServerSocket serverSocket = new ServerSocket(10000);
-        Socket socket = serverSocket.accept();
-        InputStream inputStream = socket.getInputStream();
-        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-        User user = (User)objectInputStream.readObject();
+        while (true) {
+            Socket socket = serverSocket.accept();
+            InputStream inputStream = socket.getInputStream();
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+            User user = (User)objectInputStream.readObject();
 
-        String str;
-        if (user.getUsername().equals("jiangbaiyan") && user.getPassword().equals("123456")) {
-            str = "登录成功";
-        } else {
-            str = "登录失败";
+            String str;
+            if (user.getUsername().equals("jiangbaiyan") && user.getPassword().equals("123456")) {
+                str = "登录成功";
+            } else {
+                str = "登录失败";
+            }
+            socket.shutdownInput();
+
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeUTF(str);
+
+            socket.shutdownOutput();
+
+            inputStream.close();
+            objectInputStream.close();
+            dataOutputStream.close();
+            socket.close();
         }
-        socket.shutdownInput();
-
-        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-        dataOutputStream.writeUTF(str);
-        socket.shutdownOutput();
-
-        inputStream.close();
-        objectInputStream.close();
-        dataOutputStream.close();
-        socket.close();
-        serverSocket.close();
+        // serverSocket.close();
     }
 }
