458c7bc353c43e187c12f57c5827f6b0b935eeca
baynejiang <987082332@qq.com>
2021-02-09 14:11:23 +0800

    增加子弹类

---
+++ src/tank/Bullet.java
@@ -0,0 +1,45 @@
+package src.tank;
+
+import java.awt.*;
+
+public class Bullet {
+    public static final int SPEED = 10;
+
+    public static int WIDTH = 5, HEIGHT = 5;
+
+    public int x, y;
+
+    public Dir dir;
+
+    public Bullet(int x, int y, Dir dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x, y, WIDTH, HEIGHT);
+        g.setColor(c);
+        move();
+    }
+
+    private void move() {
+        switch (dir) {
+            case LEFT:
+                x -= SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+        }
+    }
+
+}
