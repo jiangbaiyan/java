b091d94ca6932b60e1dd7933d9a1fe04502b54b1
baynejiang <baynejiang@tencent.com>
2021-11-06 12:47:12 +0800

    第一个查询接口

--- src/foodie-dev/foodie-dev-api/src/main/java/com/imooc/Application.java
+++ src/foodie-dev/foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -2,10 +2,14 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+// 扫描mybatis通用mapper所在的包
+@MapperScan(basePackages = "com.imooc.mapper")
 public class Application {
 
+    // com.imooc下的子包都会扫描进去
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
