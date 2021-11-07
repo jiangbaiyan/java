cfd4414f38c8d1a92b92f9b9ae251c67f5ba1a08
baynejiang <baynejiang@tencent.com>
2021-11-07 12:49:03 +0800

    创建用户service层

--- src/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
+++ src/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -1,8 +1,10 @@
 package com.imooc.controller;
 
 import com.imooc.service.UserService;
+import com.imooc.utils.IMOOCJSONResult;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,18 +18,18 @@ public class PassportController {
     private UserService userService;
 
     @GetMapping("/usernameIsExist")
-    public int usernameIsExist(@RequestParam String username) {
+    public IMOOCJSONResult usernameIsExist(@RequestParam String username) {
 
         if (StringUtils.isBlank(username)) {
-            return 500;
+            return IMOOCJSONResult.errorMsg("用户名不能为空");
         }
 
         boolean isExist = userService.queryUsernameIsExist(username);
         if (isExist) {
-            return 500;
+            return IMOOCJSONResult.errorMsg("用户名已存在");
         }
 
-        return 200;
+        return IMOOCJSONResult.ok();
     }
 
 }
--- src/foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/UserService.java
+++ src/foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/UserService.java
@@ -1,7 +1,12 @@
 package com.imooc.service;
 
+import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
+
 public interface UserService {
 
     // 判断用户名是否存在
     public boolean queryUsernameIsExist(String username);
+
+    public Users createUser(UserBO userBO);
 }
