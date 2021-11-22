e3d3a960f0ac9c8052c0e2759d82b61d0ad8c5fc
baynejiang <baynejiang@tencent.com>
2021-11-22 20:35:33 +0800

    参数校验

--- src/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
+++ src/foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
@@ -10,10 +10,16 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @Api(value = "用户信息接口", tags = {"用户信息相关接口"})
@@ -25,11 +31,28 @@ public class CenterUserController {
 
     @ApiOperation(value = "修改用户信息", notes = "修改用户信息", httpMethod ="GET")
     @GetMapping("update")
-    public IMOOCJSONResult update(@ApiParam(name = "userId", value = "用户id", required = true)  @RequestParam String userId, @RequestBody CenterUserBO centerUserBO, HttpServletRequest req, HttpServletResponse rsp) {
+    public IMOOCJSONResult update(@ApiParam(name = "userId", value = "用户id", required = true)  @RequestParam String userId, @RequestBody @Valid CenterUserBO centerUserBO, BindingResult result, HttpServletRequest req, HttpServletResponse rsp) {
+
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = getErrors(result);
+            return IMOOCJSONResult.errorMap(errorMap);
+        }
         Users user = centerUserService.updateUserInfo(userId, centerUserBO);
         user = setNullProperty(user);
         CookieUtils.setCookie(req, rsp, "user", JsonUtils.objectToJson(user), true);
         return IMOOCJSONResult.ok(user);
+    }
+
+    private Map<String, String> getErrors(BindingResult result) {
+        List<FieldError> fieldErrors = result.getFieldErrors();
+        Map<String, String> map = new HashMap<>();
+        for (FieldError error : fieldErrors) {
+            // 发生验证错误对应的某个属性
+            String errorField = error.getField();
+            String errorMsg = error.getDefaultMessage();
+            map.put(errorField, errorMsg);
+        }
+        return map;
     }
 
     private Users setNullProperty(Users userResult) {
